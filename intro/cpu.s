6922bab058d1d3b71a33e961d4158993d48fe6d2
sarah.laslow <sarah.laslow@gmail.com>
2019-05-04 09:45:10 -0400

    first ostep code copy

---
+++ intro/cpu.s
@@ -0,0 +1,140 @@
+	.file	"cpu.c"
+	.section	.rodata
+.LC0:
+	.string	"common.h"
+.LC1:
+	.string	"rc == 0"
+	.text
+	.globl	GetTime
+	.type	GetTime, @function
+GetTime:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	-32(%rbp), %rax
+	movl	$0, %esi
+	movq	%rax, %rdi
+	call	gettimeofday
+	movl	%eax, -36(%rbp)
+	cmpl	$0, -36(%rbp)
+	je	.L2
+	movl	$__PRETTY_FUNCTION__.2963, %ecx
+	movl	$11, %edx
+	movl	$.LC0, %esi
+	movl	$.LC1, %edi
+	call	__assert_fail
+.L2:
+	movq	-32(%rbp), %rax
+	pxor	%xmm1, %xmm1
+	cvtsi2sdq	%rax, %xmm1
+	movq	-24(%rbp), %rax
+	pxor	%xmm0, %xmm0
+	cvtsi2sdq	%rax, %xmm0
+	movsd	.LC2(%rip), %xmm2
+	divsd	%xmm2, %xmm0
+	addsd	%xmm1, %xmm0
+	movq	-8(%rbp), %rax
+	xorq	%fs:40, %rax
+	je	.L4
+	call	__stack_chk_fail
+.L4:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	GetTime, .-GetTime
+	.globl	Spin
+	.type	Spin, @function
+Spin:
+.LFB3:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movl	$0, %eax
+	call	GetTime
+	movq	%xmm0, %rax
+	movq	%rax, -8(%rbp)
+	nop
+.L6:
+	movl	$0, %eax
+	call	GetTime
+	movapd	%xmm0, %xmm1
+	subsd	-8(%rbp), %xmm1
+	pxor	%xmm0, %xmm0
+	cvtsi2sd	-20(%rbp), %xmm0
+	ucomisd	%xmm1, %xmm0
+	ja	.L6
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE3:
+	.size	Spin, .-Spin
+	.section	.rodata
+.LC3:
+	.string	"usage: cpu <string>\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB4:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movq	%rsi, -32(%rbp)
+	cmpl	$2, -20(%rbp)
+	je	.L8
+	movq	stderr(%rip), %rax
+	movq	%rax, %rcx
+	movl	$20, %edx
+	movl	$1, %esi
+	movl	$.LC3, %edi
+	call	fwrite
+	movl	$1, %edi
+	call	exit
+.L8:
+	movq	-32(%rbp), %rax
+	movq	8(%rax), %rax
+	movq	%rax, -8(%rbp)
+.L9:
+	movq	-8(%rbp), %rax
+	movq	%rax, %rdi
+	call	puts
+	movl	$1, %edi
+	call	Spin
+	jmp	.L9
+	.cfi_endproc
+.LFE4:
+	.size	main, .-main
+	.section	.rodata
+	.align 8
+	.type	__PRETTY_FUNCTION__.2963, @object
+	.size	__PRETTY_FUNCTION__.2963, 8
+__PRETTY_FUNCTION__.2963:
+	.string	"GetTime"
+	.align 8
+.LC2:
+	.long	0
+	.long	1093567616
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.10) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
